18167db4d0f90689801124c59b6bf56c5f6d6442
Kekzuke <artem.golub2019@gmail.com>
2024-11-20 01:54:06 +0300

    Update 2D Cellular Automata.pas

--- 2D Cellular Automata.pas
+++ 2D Cellular Automata.pas
@@ -1,14 +1,34 @@
 ﻿uses GraphWPF;
     
-function MatrFill(w, h, size : integer) : array [ , ] of integer; // first matrix random generation
+function MatrFill(w, h, size : integer) : array [ , ] of integer; // first matrix generation
 begin
   w := Trunc(w / size);
   h := Trunc(h / size);
   result := new integer[w,h];
   
-  for var x := 0 to w - 1 do
-    for var y := 0 to h - 1 do
-      result[x, y] := Random(0, 1);
+//  for var x := 0 to w - 1 do // random generation
+//    for var y := 0 to h - 1 do
+//      result[x, y] := Random(0, 1);
+    
+//    for var x := 0 to w - 1 do
+//      for var y := 0 to h - 1 do
+//        if (x = (w div 2)) or (y = (h div 2)) then
+//          result[x, y] := 1;
+    
+//    for var x := 0 to w - 1 do
+//      for var y := 0 to h - 1 do
+//        if x.Divs(9) then
+//          result[x, y] := 1;
+
+//    for var x := 0 to w - 1 do
+//      for var y := 0 to h - 1 do
+//        if (2 * x + y).Divs(4) then
+//          result[x, y] := 1;
+
+    for var x := 0 to w - 1 do
+      for var y := 0 to h - 1 do
+        if (x * y).Divs(22) then
+          result[x, y] := 1;
 end;
 
 
@@ -27,7 +47,9 @@ begin
       
       neighbours -= Generation[x, y]; // subtract the current cell's state
       
-      if ((Generation[x, y] = 1) and (neighbours < 2)) then // rules of automata(Game of Life)
+      // rules of automata
+      
+      if ((Generation[x, y] = 1) and (neighbours < 2)) then //Game of Life
         result[x, y] := 0
       else if ((Generation[x, y] = 1) and (neighbours > 3)) then
         result[x, y] := 0
@@ -35,6 +57,13 @@ begin
         result[x, y] := 1
       else
         result[x, y] := Generation[x, y];
+      
+//      if ((Generation[x, y] = 1) and ((neighbours in (1..2)) or (neighbours = 5))) then // Day and Night
+//        result[x, y] := 0
+//      else if ((Generation[x, y] = 0) and ((neighbours = 3) or (neighbours = 6) or (neighbours in (7..8)))) then
+//        result[x, y] := 1
+//      else
+//        result[x, y] := Generation[x, y];
     end;
 end;
 
@@ -43,7 +72,7 @@ begin
   var width := 900;
   var height := 800;
   var size := 10;
-  var FPS := 20;
+  var FPS := 10;
   
   Window.SetSize(width, height);
   Window.CenterOnScreen;
@@ -54,7 +83,8 @@ begin
   
   while True do // main cycle for drawing cells
   begin
-    Sleep(Trunc(1000 / FPS));
+  Redraw(() ->
+  begin
     Window.Clear(Colors.Gray);
     
     for var x := 0 to w step size do
@@ -71,5 +101,7 @@ begin
         if Generation[x, y] = 1 then
           Rectangle(x * size , y * size, size, size);
       end;
+  end);
+  Sleep(Trunc(1000 / FPS));
   end;
 end.
